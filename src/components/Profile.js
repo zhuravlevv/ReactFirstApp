6326be209c487b6106d513cec552d2b7a0cd5a56
Vlad Zhuravlev <vlad12.09.01.1@mail.ru>
2020-08-16 19:08:02 +0300

    main

---
+++ src/components/Profile.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Header = () =>{
+    return(
+        <header className='header'>
+            <img src='https://www.iconarchive.com/download/i94983/blackvariant/button-ui-requests-2/Minecraft-2.ico' width='40' height='40'alt='abc'/>
+        </header>
+    )
+};
+
+export default Header

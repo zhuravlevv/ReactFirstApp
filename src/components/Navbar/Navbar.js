8b6962477344c4d8477636fdcc094027fbde7b44
Vlad Zhuravlev <vlad12.09.01.1@mail.ru>
2020-08-17 21:09:27 +0300

    update navbar

--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -1,14 +1,15 @@
 import React from "react";
 import classes from './Navbar.module.css'
+import {NavLink} from "react-router-dom";
 
 const Navbar = () => {
     return(
         <nav className={classes.nav}>
             <div className={classes.item}>
-                <a href='/profile'>Profile</a>
+                <NavLink to='/profile' activeClassName={classes.activeLink}>Profile</NavLink>
             </div>
             <div className={classes.item}>
-                <a href='/dialogs'>Messages</a>
+                <NavLink to='/dialogs' activeClassName={classes.activeLink}>Messages</NavLink>
             </div>
             <div className={classes.item}>
                 <a>News</a>

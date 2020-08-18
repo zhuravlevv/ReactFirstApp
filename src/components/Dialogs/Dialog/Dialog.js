925b196014c8819eee984b5c7e4964e6b1576323
Vlad Zhuravlev <vlad12.09.01.1@mail.ru>
2020-08-18 23:35:29 +0300

    white color

--- src/components/Dialogs/Dialog/Dialog.js
+++ src/components/Dialogs/Dialog/Dialog.js
@@ -7,7 +7,7 @@ const Dialog = (props) => {
     let path = '/dialogs/' + props.id;
     return (
         <div>
-            <NavLink to={path} activeClassName={classes.dialog + ' ' + classes.active}>{props.name}</NavLink>
+            <NavLink to={path} activeClassName={classes.dialog + ' ' + classes.active} className={classes.dialog}>{props.name}</NavLink>
         </div>
     );
 };
